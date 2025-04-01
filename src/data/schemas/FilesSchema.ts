66fa6abf08bab69eb525f5f480e765a20690e75c
Nothile Moyo <Nothile1@gmail.com>
2025-04-01 20:57:11 +0100

    Building GraphQL backend model and schema for file data

---
+++ src/data/schemas/FilesSchema.ts
@@ -0,0 +1,16 @@
+/**
+ * Date created : 24/10/2024
+ *
+ * Author : Nothile Moyo
+ *
+ * Description : GraphQL file schema description file
+ *
+ * Queries and mutations for the file info in the database is hooked up here
+ *
+ */
+
+import { GraphQLSchema } from "graphql";
+
+const FilesSchema = new GraphQLSchema({});
+
+export default FilesSchema;
---
+++ src/models/files.ts
@@ -0,0 +1,45 @@
+/**
+ *
+ * Date created : 01/04/2024
+ *
+ * Author : Nothile Moyo
+ *
+ * File Model description
+ * Handles the typing for the image urls that will be used for the carousel on production
+ *
+ * This model is referenced in the resolver file as object instances which handle the ORM functionality for mongoose
+ *
+ * Note: Static methods are used here since we don't always need to instantiate our Object (e.g deletion)
+ *
+ * Note: The typing for this should be done as a generic when updating the type
+ *
+ */
+
+import mongoose, { Model } from "mongoose";
+import { FilesInterface } from "../@types/index";
+
+// Setting our types to be used in Mongoose
+type FilesModel = Model<FilesInterface, object, object>;
+
+// Define our schema for the Chat collection in the backend using Mongoose
+const fileSchema = new mongoose.Schema<FilesInterface>(
+  {
+    files: {
+      type: [
+        {
+          fileName: String,
+          filePath: String,
+        },
+      ],
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+// Create our model for exporting
+const Files = mongoose.model<FilesInterface, FilesModel>("File", fileSchema);
+
+export default Files;
